c5668046402d956e14046bd204e34865607e9377
RhodesKeegan <rhodesk@msoe.edu>
2022-03-22 15:43:44 -0500

    Cleaning up the links scraped from google searches to be usable and adding comments for what is actually going on in the code

--- src/ScraperDriver.py
+++ src/ScraperDriver.py
@@ -2,8 +2,8 @@ from scraper import Scraper
 
 
 def main():
-    scraper = Scraper('ptac repair manual')
-    scraper.scrape()
+    scraper = Scraper('ptac maintenance diy')
+    scraper.get_links()
 
 if __name__ == '__main__':
     main()
--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
+from functools import reduce
 
 class Scraper:
     def __init__(self, search_words):
         self.search_words = search_words
+        self.found_links = {}
 
-    def scrape(self):
+    def get_links(self):
+        # Setting everything up
         url = 'https://google.com/search?q=' + self.search_words
-        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-        links = soup.findAll('a')
-        unfiltered_links = []
-        for link in links:
-            unfiltered_links.append(link.get('href'))
 
+
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+
+        # Finding all links on the page and then removing non-relevant links found
+        links = soup.findAll('a')
+
+
+        links = list(filter(lambda x: '/url?q=https://www.youtube.com' not in x.get('href') and 'google.com'\
+                            not in x.get('href'), links))
+
+        # Adding links to temp list and then adding that list to the dictionary
+        cleaned_links = []
+        for link in links:
+            if 'https://' in link.get('href'):
+                href = link.get('href')
+                temp_link = href[href.find('=')+1:href.find('&')]
+                cleaned_links.append(temp_link)
+        self.found_links['Page 1'] = cleaned_links
+
+        # The next step to this is to add the function to have this go through x number of google pages and then
+        # Actually scrape the provided data
+
+    def data_scrape(self):
+        pass
