458870f545dda2c20c92f1b9447e46219a964aed
RhodesKeegan <rhodesk@msoe.edu>
2022-03-23 09:17:17 -0500

    This commit aims to actually add the scraping functionality into the script. It kind of works

--- src/ScraperDriver.py
+++ src/ScraperDriver.py
@@ -4,6 +4,7 @@ from scraper import Scraper
 def main():
     scraper = Scraper('ptac maintenance diy')
     scraper.get_links()
+    scraper.data_scrape()
 
 if __name__ == '__main__':
     main()
--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-from functools import reduce
 
 class Scraper:
     def __init__(self, search_words):
@@ -34,4 +33,44 @@ class Scraper:
         # Actually scrape the provided data
 
     def data_scrape(self):
-        pass
+        '''
+        Ight so this is how it's gonna go. First we are going to check all usual suspects for certain key words
+        In this case we are going to check all header elements, paragraph elements, div elements, span elements
+        maybe table data if needed.
+
+        After we check these for keywords, if they contain these keywords then we will further interrogate them
+        by actually getting data from it. We are also going to check all children element of the match in case
+        we will miss out on certain elements that may be present in something like a list within a div.
+        If no keywords are detected, then we ignore it and move on. This will be put into some sort of a data structure,
+        either in this method or some other method, and then that data will most likely need to be cleaned
+        '''
+
+        found_words = {}
+
+
+
+        key_words = ('filter', 'coil', 'fan', 'repair', 'maintenance')
+        link_example_one = self.found_links['Page 1'][3]
+        soup = BeautifulSoup(requests.get(link_example_one).text, 'html.parser')
+
+        for key_word in key_words:
+
+            h3 = soup.find_all('h3', text=lambda x: x and key_word in x.lower())
+            h2 = soup.find_all('h2', text=lambda x: x and key_word in x.lower())
+            p = soup.find_all('p', text=lambda x: x and key_word in x.lower())
+            div = soup.find_all('div', text=lambda x: x and key_word in x.lower())
+            h1 = soup.find_all('h1', text=lambda x: x and key_word in x.lower())
+            span = soup.find_all('span', text=lambda x: x and key_word in x.lower())
+
+            found_words['h3 ' + key_word] = h3
+            found_words['h2 ' + key_word] = h2
+            found_words['p ' + key_word] = p
+            found_words['div ' + key_word] = div
+            found_words['h1 ' + key_word] = h1
+            found_words['span ' + key_word] = span
+
+
+        # for element in elements:
+        #     print(element.find_next_sibling('p'))
+
+
