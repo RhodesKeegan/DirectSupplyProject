5227ac99fdb662a05d1a419c39582ae6b07bca88
RhodesKeegan <rhodesk@msoe.edu>
2022-03-28 15:44:43 -0500

    Obtaining some more data. Additionally, I removed the html tags from the information scraped and added more google search results

--- src/ScraperDriver.py
+++ src/ScraperDriver.py
@@ -2,7 +2,7 @@ from scraper import Scraper
 
 
 def main():
-    scraper = Scraper('ptac maintenance diy')
+    scraper = Scraper('ptac maintenance diy', 1000)
     scraper.get_links()
     scraper.data_scrape()
 
--- src/scraper.py
+++ src/scraper.py
@@ -3,20 +3,21 @@ import pandas as pd
 import requests
 
 class Scraper:
-    def __init__(self, search_words):
+    def __init__(self, search_words, num_results):
         self.search_words = search_words
-        self.found_links = {}
+        self.visited_links = []
+        self.found_links = []
+        self.num_results = num_results
 
     def get_links(self):
         # Setting everything up
-        url = 'https://google.com/search?q=' + self.search_words
+        url = f'https://google.com/search?q={self.search_words}&num={self.num_results}'
 
 
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
         # Finding all links on the page and then removing non-relevant links found
         links = soup.findAll('a')
-
 
         links = list(filter(lambda x: '/url?q=https://www.youtube.com' not in x.get('href') and 'google.com'\
                             not in x.get('href'), links))
@@ -28,55 +29,53 @@ class Scraper:
                 href = link.get('href')
                 temp_link = href[href.find('=')+1:href.find('&')]
                 cleaned_links.append(temp_link)
-        self.found_links['Page 1'] = cleaned_links
+        self.found_links = cleaned_links
 
         # The next step to this is to add the function to have this go through x number of google pages and then
-        # Actually scrape the provided data
+        # Actually scrape the provided generic_search_data
 
     def data_scrape(self):
         '''
         Ight so this is how it's gonna go. First we are going to check all usual suspects for certain key words
         In this case we are going to check all header elements, paragraph elements, div elements, span elements
-        maybe table data if needed.
+        maybe table generic_search_data if needed.
 
         After we check these for keywords, if they contain these keywords then we will further interrogate them
-        by actually getting data from it. We are also going to check all children element of the match in case
+        by actually getting generic_search_data from it. We are also going to check all children element of the match in case
         we will miss out on certain elements that may be present in something like a list within a div.
-        If no keywords are detected, then we ignore it and move on. This will be put into some sort of a data structure,
-        either in this method or some other method, and then that data will most likely need to be cleaned
+        If no keywords are detected, then we ignore it and move on. This will be put into some sort of a generic_search_data structure,
+        either in this method or some other method, and then that generic_search_data will most likely need to be cleaned
         '''
-
-
-
-
 
         key_words = ('filter', 'coil', 'fan', 'repair', 'maintenance')
         count = 0
-        for link in self.found_links['Page 1']:
-            count += 1
-            found_words = {}
+        for link in self.found_links:
+            if link not in self.visited_links:
+                count += 1
+                found_words = {}
 
-            soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+                soup = BeautifulSoup(requests.get(link).text, 'html.parser')
 
-            for key_word in key_words:
+                for key_word in key_words:
 
-                h3 = soup.find_all('h3', text=lambda x: x and key_word in x.lower())
-                h2 = soup.find_all('h2', text=lambda x: x and key_word in x.lower())
-                p = soup.find_all('p', text=lambda x: x and key_word in x.lower())
-                div = soup.find_all('div', text=lambda x: x and key_word in x.lower())
-                h1 = soup.find_all('h1', text=lambda x: x and key_word in x.lower())
-                span = soup.find_all('span', text=lambda x: x and key_word in x.lower())
-                li = soup.find_all('li', text=lambda x: x and key_word in x.lower())
+                    h3 = soup.find_all('h3', text=lambda x: x and key_word in x.lower())
+                    h2 = soup.find_all('h2', text=lambda x: x and key_word in x.lower())
+                    p = soup.find_all('p', text=lambda x: x and key_word in x.lower())
+                    div = soup.find_all('div', text=lambda x: x and key_word in x.lower())
+                    h1 = soup.find_all('h1', text=lambda x: x and key_word in x.lower())
+                    span = soup.find_all('span', text=lambda x: x and key_word in x.lower())
+                    li = soup.find_all('li', text=lambda x: x and key_word in x.lower())
 
-                found_words['h3 ' + key_word] = h3
-                found_words['h2 ' + key_word] = h2
-                found_words['p ' + key_word] = p
-                found_words['div ' + key_word] = div
-                found_words['h1 ' + key_word] = h1
-                found_words['span ' + key_word] = span
-                found_words['li ' + key_word] = li
+                    found_words['h3 ' + key_word] = map(lambda x: x.get_text(), h3)
+                    found_words['h2 ' + key_word] = map(lambda x: x.get_text(), h2)
+                    found_words['p ' + key_word] = map(lambda x: x.get_text(), p)
+                    found_words['div ' + key_word] = map(lambda x: x.get_text(), div)
+                    found_words['h1 ' + key_word] = map(lambda x: x.get_text(), h1)
+                    found_words['span ' + key_word] = map(lambda x: x.get_text(), span)
+                    found_words['li ' + key_word] = map(lambda x: x.get_text(), li)
 
-            self.store_data(found_words, count)
+                self.store_data(found_words, count)
+                self.visited_links.append(link)
 
 
 
@@ -84,6 +83,6 @@ class Scraper:
     def store_data(self, data, count):
         df = pd.DataFrame.from_dict(data, orient='index')
         df = df.transpose()
-        df.to_csv('link' + str(count) + '.csv')
+        df.to_csv('./generic_search_data/link' + str(count) + '.csv')
 
 
