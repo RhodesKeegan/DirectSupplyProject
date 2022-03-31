0ded0bb24d5710c25cbeedf5b14423b52015a5ef
Collin Quinn <71110927+ryn8raun248@users.noreply.github.com>
2022-03-30 22:24:41 -0500

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,3 +1,4 @@
+from ast import Num
 import PyPDF2, textract, nltk
 import re
 from nltk.tokenize import word_tokenize
@@ -6,6 +7,13 @@ nltk.download('stopwords')
 nltk.download('punkt')
 
 pdfreader = PyPDF2.PdfFileReader("Owners Manual Sample.pdf")
+
+loc_counter = 0
+cumulative = []
+words = []
+locations = []
+counter = 0
+
 
 def Read(startPage, endPage):
     global text
@@ -25,11 +33,49 @@ def Read(startPage, endPage):
     return text
 
 text = Read(16,16)
-words = []
+
+
 for item in text:
     if "." in item[0:]:
         location = item.find(".")
         if (location != (len(item) -1 )):
-            words.append(item)
-print(words)
+            if(item[location + 1].isupper() and item[location + 1].isalpha() and ord(item[location + 1]) != 321):
+            
+                words.append(item)
+                locations.append(counter)
+    counter += 1
+
+# print(words)
+for num in range(len(locations)):
+    if (num == 0): # first element
+
+        bunch = text[0: locations[0] + 1]
+
+    elif (num == len(locations) -1 ):
+
+        bunch = text[locations[num- 1]:]
+
+    else:
+        bunch = text[locations[num - 1] : locations[num] + 1] # 0: 1, 1:2
+
+    
+    string = ' '.join([str(item) for item in bunch])
+    if num != 0:
+        string = string.split('.', 1)
+        string = string[1]
+        import pdb; pdb.set_trace()
+        re.matc
+    else:
+        string = string.split('CausesSolutions')
+        string = string[1]
+
+    # print(string)
+    cumulative.append(string)
+    loc_counter += 1
+
+
+import pandas as pd
+
+
+pd.DataFrame(cumulative).to_excel('output3.xlsx', header=False, index=False)
 
