fb9e90c40cb73a333c7e72f6d1b62f4f9e00c0de
RhodesKeegan <rhodesk@msoe.edu>
2022-04-20 22:15:42 -0500

    Making the front end look more ~~~~beautiful~~~~~~

--- UI/src/Banner.js
+++ UI/src/Banner.js
@@ -6,8 +6,12 @@ class Banner extends React.Component {
     }
 
     reportQuestion(e){
+
         if(e.key === 'Enter'){
-            console.log('here')
+            let question = e.target.value;
+            //DO STUFF HERE FOR ADDING ACTUAL RESPONSE
+            const responseArea = document.getElementById('responseArea');
+            responseArea.style.display = 'block';
         }
     }
 
@@ -18,16 +22,21 @@ class Banner extends React.Component {
 
     render() {
         return(
-            <div className={'banner'}>
-                <h1 className={'greeting'}><b>Hello, I'm Dr. AMPS</b></h1>
-                <form>
-                    <label>
-                        <p className={'input'}>
-                            <b>Ask me about PTACs:</b>
-                        </p>
-                        <input className={'inputBox'} type={'text'} name={'name'} onKeyDown={this.reportQuestion.bind(this)}/>
-                    </label>
-                </form>
+            <div>
+                <div className={'banner'}>
+                    <h1 className={'greeting fade-in-greeting'}><b>Hello, I'm Dr. AMPS</b></h1>
+                    <form onSubmit={(e)=>{e.preventDefault()}}>
+                        <label>
+                            <p className={'input fade-in-subtext'}>
+                                <b>Ask me about PTACs:</b>
+                            </p>
+                            <input className={'inputBox'} type={'text'} name={'name'} onKeyDown={this.reportQuestion.bind(this)}/>
+                        </label>
+                    </form>
+                    <div>
+                        <p id={'responseArea'}>Response Goes Here</p>
+                    </div>
+                </div>
             </div>
 
         )
