1e843e8c64be63f3bfe472aef0a3af2f1f93c5ad
jrkohout <jrkohout64@gmail.com>
2022-04-22 09:52:05 -0500

    started porting top 5 validation answers

---
+++ openai/files/validation/top5validation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+questions = [
+    "How often should I clean the air filter?",
+    "Why is the PTAC so noisy?",
+    "How do I remove the front panel?",
+    "Why is the fan not working?",
+    "Why is there ice forming on my PTAC?"
+]
+
+validation_answers = [
+    "",
+    "The fan blades may be loose",
+    "",
+    "",
+    ""
+]
+
+ans1 = [
+    "Twice a year",
+    "You should clean the air filter every two weeks, or more often if necessary."
+    "You should clean the air filters at least every 30 days.",
+    "You should clean your air filter at least once each month",
+]
+
+ans2 = [
+
+]
