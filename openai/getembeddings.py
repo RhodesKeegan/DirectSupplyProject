b1b66a1659b6fd07702842544ef3182fa39bc82c
jrkohout <jrkohout64@gmail.com>
2022-04-22 11:58:59 -0500

    got embeddings for all top 5 validation answers supplied by each group member (OpenAI Embeddings)

--- openai/getembeddings.py
+++ openai/getembeddings.py
@@ -6,9 +6,10 @@ import datetime
 import numpy as np
 
 VALIDATION_FILE_PATH = "files/validation/qa_validation_set(2022-04-19)-model_output.csv"
+TOP_5_FILE_PATH = "files/validation/top5validation_answers.csv"
 
 
-def load_file():
+def load_validation_file():
     df_val = pd.read_csv(VALIDATION_FILE_PATH)
     assert "question" in df_val.columns
     assert "answer" in df_val.columns
@@ -16,8 +17,8 @@ def load_file():
     return df_val
 
 
-def main():
-    df_val = load_file()
+def save_validation_set_embeddings():
+    df_val = load_validation_file()
     now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     path = "files/embeddings/" + now
     os.mkdir(path)
@@ -31,6 +32,25 @@ def main():
     np.save(path + "/output.npy", output_np)
 
 
+def save_top5_embeddings():
+    df_top5 = pd.read_csv(TOP_5_FILE_PATH)
+    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    path = "files/embeddings/" + now
+    os.mkdir(path)
+    for col in df_top5.columns:
+        if col != "question":
+            vecs = get_embeddings(list(df_top5[col]), engine="text-similarity-ada-001")
+            path = "files/embeddings/" + now + "/top5_" + col + ".npy"
+            vecs_np = np.array(vecs)
+            np.save(path, vecs_np)
+
+
+def main():
+    # save_validation_set_embeddings()
+    # save_top5_embeddings()
+    pass
+
+
 if __name__ == "__main__":
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    # main()  # TODO uncomment main() to run
+    main()
