e1ed1a58798cd081a0c55a96d0f86aec544231ef
jrkohout <jrkohout64@gmail.com>
2022-04-22 12:35:09 -0500

    calculated mean similarities for top 5 validation answers

--- openai/testsimilarity.py
+++ openai/testsimilarity.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+import itertools
 
-EMBEDDINGS_DIR = "files/embeddings/2022-04-20-09-45-41/"
+# EMBEDDINGS_DIR = "files/embeddings/2022-04-20-09-45-41/"
+EMBEDDINGS_DIR = "files/embeddings/2022-04-22-11-55-58/"
 VALIDATION_FILE_PATH = "files/validation/qa_validation_set(2022-04-19)-model_output.csv"
+TOP_5_FILE_PATH = "files/validation/top5validation_answers.csv"
 
 
 def load_validation_file():
@@ -33,10 +36,31 @@ def save_validation_similarities():
     df_validation.to_csv(EMBEDDINGS_DIR + "model_output_with_scores.csv", index=False)
 
 
+def save_top5_similarities():
+    true = np.load(EMBEDDINGS_DIR + "top5_answer.npy")
+    p1 = np.load(EMBEDDINGS_DIR + "top5_person_1.npy")
+    p2 = np.load(EMBEDDINGS_DIR + "top5_person_2.npy")
+    p3 = np.load(EMBEDDINGS_DIR + "top5_person_3.npy")
+    p4 = np.load(EMBEDDINGS_DIR + "top5_person_4.npy")
+
+    # TODO get average similarities rowwise
+    all_similarities = []
+    for A, B in itertools.combinations([true, p1, p2, p3, p4], 2):  # gets all possible pairs of the 5 embeddings matrices
+        pairwise_similarities = cosine_similarities(A, B)  # pairwise similarities contains row similarities of A and B
+        all_similarities.append(pairwise_similarities)
+
+    mean_similarities = np.array(all_similarities).mean(axis=0)
+
+    df_top5 = pd.read_csv(TOP_5_FILE_PATH)
+    df_top5["mean_similarity"] = mean_similarities
+    df_top5.to_csv(EMBEDDINGS_DIR + "similarities.csv", index=False)
+
+
 def main():
     # save_validation_similarities()
-    # TODO similarities between top 5 questions from validation set
+    # save_top5_similarities()
     pass
+
 
 if __name__ == "__main__":
     main()
