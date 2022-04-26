1fcccb705ab2bbf975ba304c1efd0a24003fd429
jrkohout <jrkohout64@gmail.com>
2022-04-26 12:05:41 -0500

    calculated top 5 similarities with euclidean distances, calculated mean combination similarities between validation set answers, outputs

--- openai/testsimilarity.py
+++ openai/testsimilarity.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import itertools
 
+from openai.embeddings_utils import cosine_similarity
+
 EMBEDDINGS_DIR_VALID = "files/embeddings/2022-04-20-09-45-41/"
 EMBEDDINGS_DIR_TOP_5 = "files/embeddings/2022-04-22-11-55-58/"
 
@@ -51,22 +53,69 @@ def save_top5_similarities():
     p3 = np.load(EMBEDDINGS_DIR_TOP_5 + "top5_person_3.npy")
     p4 = np.load(EMBEDDINGS_DIR_TOP_5 + "top5_person_4.npy")
 
-    # TODO get average similarities rowwise
+    # get average similarities row-wise
     all_similarities = []
+    all_distances = []
     for A, B in itertools.combinations([true, p1, p2, p3, p4], 2):  # gets all possible pairs of the 5 embeddings matrices
         pairwise_similarities = cosine_similarities(A, B)  # pairwise similarities contains row similarities of A and B
         all_similarities.append(pairwise_similarities)
+        pairwise_distances = euclidean_distances(A, B)  # pairwise distances contains row-wise distances between A and B
+        all_distances.append(pairwise_distances)
 
     mean_similarities = np.array(all_similarities).mean(axis=0)
+    mean_distances = np.array(all_distances).mean(axis=0)
 
     df_top5 = pd.read_csv(TOP_5_FILE_PATH)
-    df_top5["mean_similarity"] = mean_similarities
+    df_top5["mean_cosine_similarity"] = mean_similarities
+    df_top5["mean_euclidean_distance"] = mean_distances
     df_top5.to_csv(EMBEDDINGS_DIR_TOP_5 + "similarities.csv", index=False)
 
 
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
+
+
+# gets mean similarity between every possible row pair in a matrix
+def get_combination_similarity(matrix):
+    all_similarities = []
+    all_distances = []
+    for a, b in itertools.combinations(matrix, 2):
+        similarity = cosine_similarity(a, b)
+        all_similarities.append(similarity)
+        distance = euclidean_distance(a, b)
+        all_distances.append(distance)
+    return np.array(all_similarities).mean(), np.array(all_distances).mean()
+
+
+def print_validation_combination_similarities():
+    answer = np.load(EMBEDDINGS_DIR_VALID + "answer.npy")
+    output = np.load(EMBEDDINGS_DIR_VALID + "output.npy")
+
+    mean_answer_similarity, mean_answer_distance = get_combination_similarity(answer)
+    mean_output_similarity, mean_output_distance = get_combination_similarity(output)
+    print("Cosine similarity:")
+    print(f"mean validation set answer combination cosine similarity:  {mean_answer_similarity}")
+    print(f"mean validation set answer combination euclidean distance: {mean_answer_distance}")
+
+    print("\nEuclidean distance:")
+    print(f"mean validation set model output combination cosine similarity:  {mean_output_similarity}")
+    print(f"mean validation set model output combination euclidean distance: {mean_output_distance}")
+
+    # OUTPUT AFTER RUNNING THIS METHOD:
+
+    # Cosine similarity:
+    # mean validation set answer combination cosine similarity:  0.7968718816358088
+    # mean validation set answer combination euclidean distance: 0.63113144087798
+    #
+    # Euclidean distance:
+    # mean validation set model output combination cosine similarity:  0.8154501507929353
+    # mean validation set model output combination euclidean distance: 0.5997398235175616
+
+
 def main():
-    save_validation_similarities()
+    # save_validation_similarities()
     # save_top5_similarities()
+    print_validation_combination_similarities()
     pass
 
 
